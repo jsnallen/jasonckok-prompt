5117e8400f224bb773c04927eb73d011ca45594e
Jason Kok <17363601+jasonckok@users.noreply.github.com>
2019-02-07 21:22:54 -0500

    Fixed location of api_key.txt for Travis CI

--- src/test/java/TestSubKeyFile.java
+++ src/test/java/TestSubKeyFile.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class TestSubKeyFile {
 
 	String workingDir = System.getProperty("user.dir");
-	File file = new File(workingDir + "\\api_key.txt");
+	File file = new File(workingDir + "/api_key.txt");
 	
 	// Test for api_key.txt is in project directory
 	@Test
